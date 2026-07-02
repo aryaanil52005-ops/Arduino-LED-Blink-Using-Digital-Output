457395d81ca18aac2ce6da4e8117b32ef507b72d
ARYA ANIL <aryaanil52005@gmail.com>
2026-07-02 23:43:07 +0530

    Add Arduino LED Blink source code

---
+++ Arduino_LED_Blink.ino
@@ -0,0 +1,13 @@
+// The built-in LED is connected to digital pin 13 on the Arduino Uno.
+
+void setup() {
+  // Initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);  // Turn the LED on
+  delay(1000);             // Wait for 1000 milliseconds (1 second)
+  digitalWrite(13, LOW);   // Turn the LED off
+  delay(1000);             // Wait for 1000 milliseconds (1 second)
+}
